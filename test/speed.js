54490ae45e01792312df83244f0fee8f6535872b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2010-06-13 02:07:27 +0300

    some speed tests added.

--- test/speed.js
+++ test/speed.js
@@ -16,10 +16,23 @@
     return _.trim("  foobar  ");
   });
 
+  JSLitmus.test('trim object-oriented', function() {
+    return _("  foobar  ").trim();
+  });
+
   JSLitmus.test('trim jQuery', function() {
     return jQuery.trim("  foobar  ");
   });
 
+
+
+  JSLitmus.test('ltrimp', function() {
+    return _.ltrim("  foobar  ", " ");
+  });
+
+  JSLitmus.test('rtrimp', function() {
+    return _.rtrim("  foobar  ", " ");
+  });
 
   JSLitmus.test('_.startsWith("foobar", "foo")', function() {
     return _.startsWith("foobar", "foo");
