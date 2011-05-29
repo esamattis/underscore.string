5573efd68bdf2f34467ffcbd5e2a19e8f7ed1ee9
J Smith <dark.panda@gmail.com>
2011-05-28 20:59:04 -0400

    Add the vsprintf method.

--- test/strings.js
+++ test/strings.js
@@ -93,6 +93,18 @@ $(document).ready(function() {
     equals(_.sprintf("%(args[0].id)d - %(args[1].name)s", {args: [{id: 824}, {name: "Hello World"}]}), "824 - Hello World", 'Named replacements with arrays work');
   });
 
+  test("Strings: vsprintf", function() {
+    // Should be very tested function already.  Thanks to
+    // http://www.diveintojavascript.com/projects/sprintf-for-javascript
+    equals(_.vsprintf("Hello %s", ["me"]), "Hello me", 'basic');
+    equals(_("Hello %s").vsprintf(["me"]), "Hello me", 'object');
+    equals(_("hello %s").chain().vsprintf(["me"]).capitalize().value(), "Hello me", 'Chaining works');
+    equals(_.vsprintf("%.1f", [1.22222]), "1.2", 'round');
+    equals(_.vsprintf("%.1f", [1.17]), "1.2", 'round 2');
+    equals(_.vsprintf("%(id)d - %(name)s", [{id: 824, name: "Hello World"}]), "824 - Hello World", 'Named replacements work');
+    equals(_.vsprintf("%(args[0].id)d - %(args[1].name)s", [{args: [{id: 824}, {name: "Hello World"}]}]), "824 - Hello World", 'Named replacements with arrays work');
+  });
+
   test("Strings: startsWith", function() {
     ok(_("foobar").startsWith("foo"), 'foobar starts with foo');
     ok(!_("oobar").startsWith("foo"), 'oobar does not start with foo');
