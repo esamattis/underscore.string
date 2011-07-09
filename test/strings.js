cde5f0268aa65c1249c7e4c977a269a27723605f
Pavel Pravosud <rwz@duckroll.ru>
2011-07-09 21:24:34 +0700

    reformatted code to soft tabs 2, updated sprint ti 0.7-beta1, closes issue #24

--- test/strings.js
+++ test/strings.js
@@ -89,6 +89,19 @@ $(document).ready(function() {
     equals(_("hello %s").chain().sprintf("me").capitalize().value(), "Hello me", 'Chaining works');
     equals(_.sprintf("%.1f", 1.22222), "1.2", 'round');
     equals(_.sprintf("%.1f", 1.17), "1.2", 'round 2');
+    equals(_.sprintf("%(id)d - %(name)s", {id: 824, name: "Hello World"}), "824 - Hello World", 'Named replacements work');
+    equals(_.sprintf("%(args[0].id)d - %(args[1].name)s", {args: [{id: 824}, {name: "Hello World"}]}), "824 - Hello World", 'Named replacements with arrays work');
+  });
+  
+  
+  test("Strings: vsprintf", function() {
+    equals(_.vsprintf("Hello %s", ["me"]), "Hello me", 'basic');
+    equals(_("Hello %s").vsprintf(["me"]), "Hello me", 'object');
+    equals(_("hello %s").chain().vsprintf(["me"]).capitalize().value(), "Hello me", 'Chaining works');
+    equals(_.vsprintf("%.1f", [1.22222]), "1.2", 'round');
+    equals(_.vsprintf("%.1f", [1.17]), "1.2", 'round 2');
+    equals(_.vsprintf("%(id)d - %(name)s", [{id: 824, name: "Hello World"}]), "824 - Hello World", 'Named replacement works');
+    equals(_.vsprintf("%(args[0].id)d - %(args[1].name)s", [{args: [{id: 824}, {name: "Hello World"}]}]), "824 - Hello World", 'Named replacement with arrays works');
   });
 
   test("Strings: startsWith", function() {
