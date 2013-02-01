69dc02de6aee4715ee0b1e22b58c1f16d5823e77
Pavel Pravosud <pavel@pravosud.com>
2013-02-01 22:11:24 +0700

    Added a function for natural sorting
    Closes #177

--- test/strings.js
+++ test/strings.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
 
   module('String extensions');
 
+  test('Strings: naturalSort', function() {
+    var arr =  ['foo2', 'foo1', 'foo10', 'foo30', 'foo100', 'foo10bar'],
+      sorted = ['foo1', 'foo2', 'foo10', 'foo10bar', 'foo30', 'foo100'];
+    deepEqual(arr.sort(_.naturalCmp), sorted);
+  });
+
   test('Strings: trim', function() {
     equal(_.trim(123), '123', 'Non string');
     equal(_(' foo').trim(), 'foo');
