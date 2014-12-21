c882b154506b07de3f17d3d765df69c94b83faf3
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-12-21 12:57:18 +0200

    Put all build artifacts to dist/
    And rename node entry point to index.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require('gulp-param')(require('gulp'), process.argv),
   replace = require('gulp-replace'),
   rename = require('gulp-rename'),
   browserify = require('gulp-browserify'),
-  SRC = 'underscore.string.js',
-  SRC_COMPILED = 'underscore.string.compiled.js',
+  SRC = 'index.js',
   DEST = 'dist',
+  SRC_COMPILED = 'underscore.string.js',
   MIN_FILE = 'underscore.string.min.js',
   TEST_SUITES = ['test/test.html', 'test/test_standalone.html', 'test/test_underscore/index.html'],
   VERSION_FILES = ['./package.json', './component.json'];
@@ -24,8 +24,8 @@ gulp.task('browserify', function() {
       detectGlobals: true,
       standalone: 'underscore.string'
     }))
-    .pipe(rename('underscore.string.compiled.js'))
-    .pipe(gulp.dest('./'));
+    .pipe(rename('underscore.string.js'))
+    .pipe(gulp.dest(DEST));
 });
 
 gulp.task('clean', function() {
