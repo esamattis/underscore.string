6885cbd2f88f6270d6c156cccdc924049755f75f
Luca Mussi <ing.luca.mussi@gmail.com>
2015-02-07 20:19:00 +0100

    added package.js metadata file for Meteor

--- package.js
+++ package.js
@@ -1,35 +1,13 @@
 // package metadata file for Meteor.js
-var packageName = 'underscorestring:underscore.string'; // https://atmospherejs.com/underscorestring/underscore-string
-var where = ''; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = "2.4.0";
 
 Package.describe({
-  "name": packageName,
-  "summary": 'underscore.string (official) - string manipulation helpers.',
-  "version": version,
-  "git": 'https://github.com/epeli/underscore.string.git'
-});
-
-Npm.depends({
-  "underscore.string": version
+  'name': 'underscorestring:underscore.string',
+  'summary': 'underscore.string (official): String manipulation extensions for Underscore.js javascript library.',
+  'version': '2.4.0',
+  'git': 'https://github.com/epeli/underscore.string.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
-  api.use('underscore', {weak: true});
-  api.addFiles([
-    'dist/underscore.string.min.js'
-    ], ["client"]
-  );
-  api.addFiles([
-    'meteor/export.js'
-    ], ["client", "server"]
-  );
-  api.export('_s');
-});
-
-Package.onTest(function (api) {
-  api.use(packageName, where);
-  api.use('tinytest', where);
-  api.addFiles('meteor/test.js', where);
+  api.versionsFrom('METEOR@1.0');
+  api.addFiles('dist/underscore.string.min.js');
 });
