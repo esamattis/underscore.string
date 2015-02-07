16eecce0284e857f9b0c7ef61ac41f06e068f3cb
Luca Mussi <ing.luca.mussi@gmail.com>
2015-02-07 20:18:08 +0100

    removed meteor folder

--- meteor/export.js
+++
@@ -1,7 +0,0 @@
-if ( Meteor.isClient ) {
-  _s = _.str;
-}
-
-if ( Meteor.isServer ) {
-  _s = Npm.require("underscore.string");
-}
--- meteor/test.js
+++
@@ -1,4 +0,0 @@
-Tinytest.add('underscore.string.is', function(test) {
-  test.instanceOf(_s, Object);
-  test.equal(_s.capitalize("test"), "Test");
-});
