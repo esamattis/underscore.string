63450a0f24c585a09e614527981bf55f66d850bc
az7arul <az7arul@gmail.com>
2015-03-16 21:50:23 +0600

    fix underscored method for single letter worlds

--- underscored.js
+++ underscored.js
@@ -1,5 +1,5 @@
 var trim = require('./trim');
 
 module.exports = function underscored(str) {
-  return trim(str).replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
+  return trim(str).replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/([A-Z\d]+)([A-Z][a-z])/,'$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
 };
