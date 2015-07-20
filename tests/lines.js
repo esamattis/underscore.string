d5258019a8b8df69a751acd227659d8121fa66e6
Alessandro Di Felice <alessandro@uniregistry.com>
2015-07-20 16:02:54 -0500

    lines: add support to CR ending lines

--- tests/lines.js
+++ tests/lines.js
@@ -5,12 +5,16 @@ var lines = require('../lines');
 
 test('#lines', function() {
   equal(lines('Hello\nWorld').length, 2);
+  equal(lines('Hello\rWorld').length, 2);
   equal(lines('Hello World').length, 1);
+  equal(lines('\r\n\n\r').length, 4);
+  equal(lines('Hello\r\r\nWorld').length, 3);
+  equal(lines('Hello\r\rWorld').length, 3);
   equal(lines(123).length, 1);
   deepEqual(lines(''), ['']);
   deepEqual(lines(null), []);
   deepEqual(lines(undefined), []);
-  deepEqual(lines('Hello\rWorld'), ['Hello\rWorld']);
+  deepEqual(lines('Hello\rWorld'), ['Hello', 'World']);
   deepEqual(lines('Hello\r\nWorld'), ['Hello', 'World']);
 });
 
