c2b688e62552d0822b90e8af7812fb44646fb127
Alessandro Di Felice <alessandro@uniregistry.com>
2015-07-20 15:23:01 -0500

    Changed lines regex to a nicer one (stevenbenner)

--- tests/lines.js
+++ tests/lines.js
@@ -7,6 +7,9 @@ test('#lines', function() {
   equal(lines('Hello\nWorld').length, 2);
   equal(lines('Hello\rWorld').length, 2);
   equal(lines('Hello World').length, 1);
+  equal(lines('\r\n\n\r').length, 4);
+  equal(lines('Hello\r\r\nWorld').length, 3);
+  equal(lines('Hello\r\rWorld').length, 3);
   equal(lines(123).length, 1);
   deepEqual(lines(''), ['']);
   deepEqual(lines(null), []);
