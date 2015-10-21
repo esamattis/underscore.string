414507efef4de3ca95fb3967cc3adf0d09d3e150
Julián Salgado <julituti@gmail.com>
2015-10-21 20:46:25 +0200

    Add map method: Creates a new string with the results of calling a provided function on every character of the given string.

--- exports.js
+++ exports.js
@@ -2,7 +2,7 @@ module.exports = function() {
   var result = {};
 
   for (var prop in this) {
-    if (!this.hasOwnProperty(prop) || prop.match(/^(?:include|contains|reverse|join)$/)) continue;
+    if (!this.hasOwnProperty(prop) || prop.match(/^(?:include|contains|reverse|join|map)$/)) continue;
     result[prop] = this[prop];
   }
 
