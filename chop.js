4a01081c3f154813a70844129eb4234846439430
evgenyzinoviev <me@ch1p.com>
2015-11-27 01:04:02 +0100

    Fix chop for multiline strings

--- chop.js
+++ chop.js
@@ -2,5 +2,5 @@ module.exports = function chop(str, step) {
   if (str == null) return [];
   str = String(str);
   step = ~~step;
-  return step > 0 ? str.match(new RegExp('.{1,' + step + '}', 'g')) : [str];
+  return step > 0 ? str.match(new RegExp('[^]{1,' + step + '}', 'gm')) : [str];
 };
