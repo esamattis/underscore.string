7c03e8383e6d0a6d50d82f0a7a9a2e12453cddee
Jorik Tangelder <jorik@blendle.nl>
2016-01-18 17:39:05 +0100

    Update sprintf and vsprintf to make use of the sprintf-js package.
    The owner of the current implementation of sprintf has continued development and improved the implementation. It makes sense to use of his package on npm instead of using an out-of-date copy.

--- vsprintf.js
+++ vsprintf.js
@@ -1,6 +1,1 @@
-var sprintf = require('./sprintf');
-
-module.exports = function vsprintf(fmt, argv) {
-  argv.unshift(fmt);
-  return sprintf.apply(null, argv);
-};
+module.exports = require('sprintf-js').vsprintf;
