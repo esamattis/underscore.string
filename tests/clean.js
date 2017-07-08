75f95d08e34fdc0bb249b408b6319749093ee436
mahdavipanah <h.mahdavipanah@gmail.com>
2017-07-08 04:40:42 +0430

    Fix typo in 'clean' tests cases

--- tests/clean.js
+++ tests/clean.js
@@ -6,7 +6,7 @@ test('#clean', function() {
   equal(clean(' foo    bar   '), 'foo bar');
   equal(clean('foo bar'), 'foo bar', "cleaning a string not containing multiple spaces returns string itself");
   equal(clean(123), '123');
-  equal(clean(''), '', 'claning empty string returns empty string');
-  equal(clean(null), '', 'claning null returns empty string');
-  equal(clean(undefined), '', 'claning undefined returns empty string');
+  equal(clean(''), '', 'cleaning empty string returns empty string');
+  equal(clean(null), '', 'cleaning null returns empty string');
+  equal(clean(undefined), '', 'cleaning undefined returns empty string');
 });
