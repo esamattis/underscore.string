149d0d398300bfc0f55b9896f892b6ff215a62de
mahdavipanah <h.mahdavipanah@gmail.com>
2017-07-08 12:22:57 +0430

    Change double quoted message to single quoted

--- tests/clean.js
+++ tests/clean.js
@@ -4,8 +4,8 @@ var clean = require('../clean');
 
 test('#clean', function() {
   equal(clean(' foo    bar   '), 'foo bar');
-  equal(clean('foo bar'), 'foo bar', "cleaning a string not containing multiple spaces returns string itself");
-  equal(clean(123), '123', "cleaning number returns it's string representation");
+  equal(clean('foo bar'), 'foo bar', 'cleaning a string not containing multiple spaces returns string itself');
+  equal(clean(123), '123', 'cleaning number returns it\'s string representation');
   equal(clean(''), '', 'cleaning empty string returns empty string');
   equal(clean(null), '', 'cleaning null returns empty string');
   equal(clean(undefined), '', 'cleaning undefined returns empty string');
