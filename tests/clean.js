9c10a910c7ec92293a5a04aadf7e8c44ccd78571
mahdavipanah <h.mahdavipanah@gmail.com>
2017-07-08 04:39:32 +0430

    Test 'clean' with a string not containing multiple spaces

--- tests/clean.js
+++ tests/clean.js
@@ -4,6 +4,7 @@ var clean = require('../clean');
 
 test('#clean', function() {
   equal(clean(' foo    bar   '), 'foo bar');
+  equal(clean('foo bar'), 'foo bar', "cleaning a string not containing multiple spaces returns string itself");
   equal(clean(123), '123');
   equal(clean(''), '', 'claning empty string returns empty string');
   equal(clean(null), '', 'claning null returns empty string');
