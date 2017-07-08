d7e8142dfb620572483534af3f11830e8032b5a5
mahdavipanah <h.mahdavipanah@gmail.com>
2017-07-08 04:43:04 +0430

    Add message for 'clean' number test case

--- tests/clean.js
+++ tests/clean.js
@@ -5,7 +5,7 @@ var clean = require('../clean');
 test('#clean', function() {
   equal(clean(' foo    bar   '), 'foo bar');
   equal(clean('foo bar'), 'foo bar', "cleaning a string not containing multiple spaces returns string itself");
-  equal(clean(123), '123');
+  equal(clean(123), '123', "cleaning number returns it's string representation");
   equal(clean(''), '', 'cleaning empty string returns empty string');
   equal(clean(null), '', 'cleaning null returns empty string');
   equal(clean(undefined), '', 'cleaning undefined returns empty string');
